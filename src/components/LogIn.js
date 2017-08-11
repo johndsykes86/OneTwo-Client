b35ce4d49b7b48365de421b5c05f3a47b939552f
Johnathon Sykes <hireme@mrjsykes.com>
2017-08-11 04:51:10 -0700

    added login styling

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,43 +1,48 @@
 import React from 'react'
 import auth from '../auth'
-import { Redirect } from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
+import { Container, Button, Checkbox, Form } from 'semantic-ui-react'
 
-class LogIn extends React.Component{
+class LogIn extends React.Component {
 
   state = {
     shouldRedirect: false
   }
 
- handleLogInSubmit(evt){
-   evt.preventDefault()
-   const formData = {
-    email: this.refs.email.value,
-    password: this.refs.password.value
-   }
-   auth.logIn(formData).then((user)=>{
-     if (user){
-      this.props.onLogIn()
-      this.setState({shouldRedirect: true})
-     }
-   })
+  handleLogInSubmit(evt) {
+    evt.preventDefault()
+    const formData = {
+      email: this.refs.email.value,
+      password: this.refs.password.value
+    }
+    auth.logIn(formData).then((user) => {
+      if (user) {
+        this.props.onLogIn()
+        this.setState({shouldRedirect: true})
+      }
+    })
 
- }
- render() {
-   return (
-     this.state.shouldRedirect
-     ? <Redirect to='/' />
-     : (
-       <div className="LogIn">
-         <h1>Log In</h1>
-         <form onSubmit={this.handleLogInSubmit.bind(this)}>
-           <input ref="email" type="text" placeholder="Email" /><br/>
-           <input ref="password" type="password" placeholder="Password" /><br/>
-           <button>Log In</button>
-         </form>
-       </div>
-     )
-   )
- }
+  }
+  render() {
+    return (this.state.shouldRedirect
+      ? <Redirect to='/stadiums'/>
+      : (
+        <Container>
+          <Form success error onSubmit={this.handleLogInSubmit.bind(this)}>
+            <Form.Field>
+              <label>Email</label>
+              <input ref="email" placeholder='Email'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input ref="password" type="password" placeholder='Password'/>
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        </Container>
+
+      ))
+  }
 
 }
 
