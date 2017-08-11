6b30d1eb439204a8d687feeed1ebb61c2610582f
Johnathon Sykes <hireme@mrjsykes.com>
2017-08-11 04:39:42 -0700

    removed commented out coode from NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,26 +39,3 @@ export default class NavBar extends Component {
     )
   }
 }
-
-// const NavBar = (props) => {
-//   return (
-//     <ul className="NavBar">
-//       <li><NavLink exact to='/'>Home</NavLink></li>
-//       {props.currentUser
-//         ? (
-//           <div>
-//             <li><NavLink to='/stadiums'>Stadiums</NavLink></li>
-//             <li><NavLink to='/profile/'>Profile</NavLink></li>
-//             <li><NavLink to='/logout'>Log Out</NavLink></li>
-//           </div>
-//         )
-//         : (
-//           <div>
-//             <li><NavLink to='/login'>Log In</NavLink></li>
-//             <li><NavLink to='/signup'>Sign Up</NavLink></li>
-//           </div>
-//         )
-//       }
-//     </ul>
-//   )
-// }
