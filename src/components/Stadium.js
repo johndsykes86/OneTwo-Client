669a1d45cdc1f64c9ab10c7c6343127e8c7e3ed9
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-06 12:43:07 -0700

    Additional styling to stadium view component

--- src/components/Stadium.js
+++ src/components/Stadium.js
@@ -2,7 +2,7 @@ import React from 'react'
 import auth from '../auth'
 import CheckIn from '../components/CheckIn'
 import ShowCheckins from '../components/ShowCheckIns'
-import {Container, Image, Statistic } from 'semantic-ui-react'
+import {Container, Image, Statistic, Header, Divider } from 'semantic-ui-react'
 
 class Stadium extends React.Component{
   constructor() {
@@ -31,18 +31,45 @@ class Stadium extends React.Component{
 
   render(){
     return(
-      <Container className ="View">
+      <Container className ="view">
+        {console.log(this.state.stadium)}
         <Image src={this.state.stadium.picURL}/>
       <Container key={1} className="single-stadium-view">
-        <h1 key={1}>{this.state.stadium.stadiumName}</h1>
-        <Statistic.Group size="mini">
+        <div className="stadium-header">
+          <Header as ='h1' textAlign="center" key={1}>{this.state.stadium.stadiumName}</Header>
+          <Header as ='h3' textAlign="center" dividing key={2}>{this.state.stadium.team}</Header>
+
+        </div>
+        <Statistic.Group size="mini" className="stadium-stats" widths='four'>
+
 
               <Statistic>
-                <Statistic.Value>{this.state.stadium.team}</Statistic.Value>
-                <Statistic.Label>Team</Statistic.Label>
+                <Statistic.Value>{this.state.stadium.location}</Statistic.Value>
+                <Statistic.Label>Location</Statistic.Label>
               </Statistic>
+
+              <Statistic>
+                <Statistic.Value>{this.state.stadium.yearBuilt}</Statistic.Value>
+                <Statistic.Label>Year Built</Statistic.Label>
+              </Statistic>
+
+              <Statistic>
+                <Statistic.Value>{this.state.stadium.playingSurface}</Statistic.Value>
+                <Statistic.Label>Playing Surface</Statistic.Label>
+              </Statistic>
+
+              <Statistic>
+                <Statistic.Value>{this.state.stadium.capacity}</Statistic.Value>
+                <Statistic.Label>capacity</Statistic.Label>
+              </Statistic>
+
+
+
+
         </Statistic.Group>
+        <Divider/>
       </Container>
+      <Header as='h3'>Check-In</Header>
       <CheckIn parent={this}/>
       <ShowCheckins parent={this}/>
     </Container>
