3d5f65343bce6e0225143a113b2d45a50362f5a2
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-06 11:38:03 -0700

    added stadium image to Stadium component.

--- src/components/Stadium.js
+++ src/components/Stadium.js
@@ -2,7 +2,7 @@ import React from 'react'
 import auth from '../auth'
 import CheckIn from '../components/CheckIn'
 import ShowCheckins from '../components/ShowCheckIns'
-import {Container } from 'semantic-ui-react'
+import {Container, Image } from 'semantic-ui-react'
 
 class Stadium extends React.Component{
   constructor() {
@@ -32,7 +32,8 @@ class Stadium extends React.Component{
   render(){
     return(
       <Container>
-      <div key={1} className="single-stadium-view">
+        <Image src={this.state.stadium.picURL}/>
+      <Container key={1} className="single-stadium-view">
         <h1 key={1}>{this.state.stadium.stadiumName}</h1>
         <ul>
           <li>Team: {this.state.stadium.team}</li>
@@ -40,9 +41,9 @@ class Stadium extends React.Component{
           <li>Location: {this.state.stadium.location}</li>
           <li>Year Built: {this.state.stadium.yearBuilt}</li>
         </ul>
-        <CheckIn parent={this}/>
-        <ShowCheckins parent={this}/>
-      </div>
+      </Container>
+      <CheckIn parent={this}/>
+      <ShowCheckins parent={this}/>
     </Container>
     )
   }
